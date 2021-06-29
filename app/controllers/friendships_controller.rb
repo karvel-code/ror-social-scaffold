8f29c2a586d470c4d545af479d577978f0f1727f
karvel-code <velgithinji@gmail.com>
2021-06-29 14:58:12 +0300

    Modify user controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,8 +4,7 @@ class FriendshipsController < ApplicationController
     end
 
     def create
-        @friendship = current_user.pending_friendships.build(user_id: params[:user][:user_id],
-                                                             friend_id: params[:user][:friend_id])
+       @friendship = current_user.friend_requests.build(user_id: params[:user][:user_id], friend_id: params[:user][:friend_id])
         if @friendship.save
           redirect_to users_path, notice: 'Friend requested'
         else
